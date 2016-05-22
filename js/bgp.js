19df38376c5fba4a510227f80b6ba49349a9ca10
Patric Eberle <line-in@derverstaerker.ch>
2016-05-22 17:01:01 +0200

    Adding player interaction

--- js/bgp.js
+++ js/bgp.js
@@ -6,63 +6,112 @@ $(function () {
     var SELECTOR_ATMOSPHERE_OBJ = '#atmosphere-object';
     var SELECTOR_ATMOSPHERE_PLAYER = '#atmosphere-player';
     var SELECTOR_MUSIC_OBJ = '#music-object';
+    var SELECTOR_MUSIC_PLAYER = '#music-player';
+    var SELECTOR_OBJ = '.jp';
+    var PAUSE_OTHER_DELAY = 5000;
     var swfPath = '../lib/jplayer/dist/jplayer';
+    var playlist = {};
 
-    function createPlaylist(playlist, player, options) {
-        var myPlaylist = new jPlayerPlaylist({
-            jPlayer: player,
-            cssSelectorAncestor: playlist
-        }, [
+    function getPlaylist(player) {
+        var id = player.attr('id');
+
+        return playlist['#' + id];
+    }
+
+    function canplay(e) {
+        var player = $(this);
+        var jPlayerPlaylist = getPlaylist(player);
+        var media = e.jPlayer.status.media;
+
+        // Delay track if defined so
+        if (media.delay) {
+            jPlayerPlaylist.pause();
+
+            setTimeout(function () {
+                jPlayerPlaylist.play();
+            }, media.delay);
+        }
+    }
+
+    function play(e) {
+        var player = $(this);
+        var otherPlayer = $(SELECTOR_OBJ).not(player);
+        var media = e.jPlayer.status.media;
+
+        if (media.pauseOther) {
+            // TODO: Only if playing, remove timeouts
+            otherPlayer
+                .jPlayerFade()
+                .out(PAUSE_OTHER_DELAY, null, 0, function() {
+                    getPlaylist(otherPlayer).pause();
+                });
+        }
+
+    }
+
+    function createPlaylist(playerObj, playerEl, options) {
+        playlist[playerObj] = new jPlayerPlaylist({
+                jPlayer: playerObj,
+                cssSelectorAncestor: playerEl
+            }, options.tracks,
             {
-                title: 'Cro Magnon Man',
-                artist: 'The Stark Palace',
-                mp3: 'http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3',
-                oga: 'http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg'
-            }, {
-                title: 'Lismore',
-                artist: 'Unknown',
-                mp3: 'http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3'
-            }
-        ], {
-            playlistOptions: {
-                enableRemoveControls: true
-            },
-            swfPath: '../lib/jplayer/jplayer',
-            supplied: 'webmv, ogv, m4v, oga, mp3',
+                swfPath: swfPath,
+                supplied: 'webmv, ogv, m4v, oga, mp3',
+                useStateClassSkin: true,
+                autoBlur: false,
+                smoothPlayBar: true,
+                keyEnabled: true
+            });
+    }
+
+    function createPlayer(playerObj, playerEl, customSettings) {
+        var settings = {
+            supplied: 'mp3',
+            wmode: 'window',
             useStateClassSkin: true,
             autoBlur: false,
             smoothPlayBar: true,
             keyEnabled: true,
-            audioFullScreen: true
-        });
+            remainingDuration: true,
+            toggleDuration: true,
+            canplay: canplay,
+            play: play
+        };
+
+        $.extend(settings, customSettings);
+
+        $(playerObj).jPlayer(settings);
+
+        createPlaylist(playerObj, playerEl, customSettings);
     }
 
-    function createPlayer(player, playlist, options) {
-        $(player).jPlayer(options);
-
-        createPlaylist(playlist, player, options);
-    }
-
-    function createAdtmospherePlaylist() {
-
-    }
-
-    function createMusicPlayer() {
-        $(SELECTOR_MUSIC_OBJ).jPlayer({
-            cssSelectorAncestor: '#atmosphere-player'
-        });
-    }
+    createPlayer(SELECTOR_MUSIC_OBJ, SELECTOR_MUSIC_PLAYER, {
+        cssSelectorAncestor: SELECTOR_MUSIC_PLAYER,
+        tracks: [
+            {
+                title: 'Anvengers',
+                // artist: 'The Stark Palace',
+                delay: 5000,
+                mp3: '/Users/Shared/Projekte/boardgameplayer/music/music/® Avengers, The Score - 19 - Alan Silvestri - The Avengers.mp3',
+                pauseOther: true
+            }, {
+                title: 'Casino Royale',
+                artist: 'Unknown',
+                delay: 3000,
+                mp3: '/Users/Shared/Projekte/boardgameplayer/music/music/® Casino Royale [Disc 1] - 02 - Chris Cornell - You Know My Name.mp3'
+            }
+        ]
+    });
 
     createPlayer(SELECTOR_ATMOSPHERE_OBJ, SELECTOR_ATMOSPHERE_PLAYER, {
-        supplied: 'mp3',
-        wmode: 'window',
-        useStateClassSkin: true,
-        autoBlur: false,
-        smoothPlayBar: true,
-        keyEnabled: true,
-        remainingDuration: true,
-        toggleDuration: true,
-        cssSelectorAncestor: SELECTOR_ATMOSPHERE_PLAYER
+        cssSelectorAncestor: SELECTOR_ATMOSPHERE_PLAYER,
+        tracks: [
+            {
+                title: 'Dome City Centre',
+                // artist: 'The Stark Palace',
+                mp3: '/Users/Shared/Projekte/boardgameplayer/music/ambiente/43_Dome_City_Center.mp3',
+                // pauseOther: true
+            }
+        ]
     });
-    createMusicPlayer();
 });
